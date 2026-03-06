266688de11bca09029641083aee655cf40a5c7d8
jouleion <124351760+jouleion@users.noreply.github.com>
2026-03-06 09:10:58 +0100

    Platformio setup (arduino ide, but fast and in vscode)

--- src/TimeOfFlightSensor.h
+++ src/TimeOfFlightSensor.h
@@ -1,5 +1,9 @@
 #pragma once
 
+// This is the VL53L7CX ToF Sensor
+// https://www.st.com/resource/en/datasheet/vl53l7cx.pdf
+//  matrix of 8x8 zones (64 zones) and can work at fast speeds (60 Hz) up to 350 cm.
+
 
 // Simplified sensor class for TOF sensor
 class TimeOfFlightSensor {
